a6d850c4fe4f2b687b6783311377e584601e9996
Jared Stromberg <jaredstromberg@gmail.com>
2026-08-15 22:37:09 -0600

    cleanup

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -71,6 +71,7 @@ git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+    CONFIG_FEATURE_SUID_CONFIG
 else
     cd busybox
 fi
@@ -81,12 +82,16 @@ make defconfig
 make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
 make CONFIG_PREFIX="${OUTDIR}/rootfs" ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
+# set uid root 
+chown root:root /bin/busybox
+chmod u+s /bin/busybox
+
 
 # TODO: Add library dependencies to rootfs
 # 1. Find needed dependencies
 echo "Library dependencies"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
+${CROSS_COMPILE}readelf -a /bin/busybox | grep "program interpreter"
+${CROSS_COMPILE}readelf -a /bin/busybox | grep "Shared library"
 # 2. Copy the files from sysroot into our root file system 
 # program interpreter needs to be in /lib
 # shared libraries need to be in /lib64
